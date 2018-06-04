962eedd3a6bb3938022e236aaadd75cd4bc072e7
Jo-Errol <jona071@hotmail.fr>
2018-06-04 10:32:06 +0200

    exo 7 reponse dans exo_07_c

---
+++ semaine zero/jour 4/exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+
+#La difference entre les 3 programmes est ce qui s'affiche pour l'utilisateur.
+#Le 3eme programme ne renseigne pas sur la valeur à entrer.
+# Le second indique ce qu'il faut entrer et signal le lieu de reponse.
