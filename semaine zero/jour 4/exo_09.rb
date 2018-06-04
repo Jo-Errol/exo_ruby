ef565df6997f73c91f22ab26ba9aeab095b04eaa
Jo-Errol <jona071@hotmail.fr>
2018-06-04 10:41:40 +0200

    exercice 9 dit bonjour

---
+++ semaine zero/jour 4/exo_09.rb
@@ -0,0 +1,7 @@
+puts "Quel est ton prenom ?"
+print "> "
+user_firstname = gets.chomp
+puts "Quel est ton nom de famille ?"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, " + user_firstname + " " + user_name + " !"
