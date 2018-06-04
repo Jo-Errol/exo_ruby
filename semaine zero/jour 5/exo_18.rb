bf699a79c05d9b567a4ed0cb1ece9bc3965d36bd
Jo-Errol <jona071@hotmail.fr>
2018-06-04 16:22:29 +0200

    où comment galérer à cause d'un = en trop

--- semaine zero/jour 5/exo_18.rb
+++ semaine zero/jour 5/exo_18.rb
@@ -3,12 +3,10 @@ email = []
 n = 00
 
 begin
-	new_email = ""nom.prenom" + n.to_s + "@email.fr""
+	new_email = "nom.prenom" + n.to_s + "@email.fr"
 	new_email = new_email.to_s
 	email << new_email
 	n += 1
 end while (n <= 50)
-
-# pour afficher le tableau retirer le # à la ligne suivante
 
 puts email [0..49]
