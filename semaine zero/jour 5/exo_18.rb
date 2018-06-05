568dcbe391b117e815bf9f4f47f5fdf410c42d71
Jo-Errol <jona071@hotmail.fr>
2018-06-05 20:13:26 +0200

    test commit

--- semaine zero/jour 5/exo_18.rb
+++ semaine zero/jour 5/exo_18.rb
@@ -3,12 +3,12 @@ email = []
 n = 00
 
 begin
- 
 	new_email = "nom.prenom#{n.to_s}@email.fr"
 	new_email = new_email.to_s
 	email << new_email.to_s
 	n += 1
-end while (n <= 50)
 
- 
+
+end while (n <= 50)
+n = 393939;
 puts email [0..49]
