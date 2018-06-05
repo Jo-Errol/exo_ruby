cca273cd115430ebcfb4e98dae39ca4bb9d00701
Jo-Errol <jona071@hotmail.fr>
2018-06-05 09:14:53 +0200

    où comment galéré à cause d'un =

---
+++ semaine zero/jour 5/exo_18.rb
@@ -0,0 +1,12 @@
+email = []
+
+n = 00
+
+begin
+	new_email = "nom.prenom" + n.to_s + "@email.fr"
+	new_email = new_email.to_s
+	email << new_email
+	n += 1
+end while (n <= 50)
+
+puts email [0..49]
